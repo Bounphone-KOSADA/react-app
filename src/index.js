1d705528196397c4e16c98cc31981f0c600e6d01
Bounphone KOSADA <bounphonexkosada@gmail.com>
2023-12-23 17:17:32 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// Creating class components
-class HelloComponent extends React.Component {
-  render() {
-    return (
-       <h1>HelloComponent</h1>
-    );
-  }
-}
-
-root.render(<HelloComponent></HelloComponent>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
